b751edfd4e24885af9889925f3e63ca0b4ea60bb
ltsimps <ltsimpso@me.com>
2017-05-12 20:02:23 -0500

    Z005.1 Added More test

--- test/talker_test.cpp
+++ test/talker_test.cpp
@@ -85,6 +85,142 @@ TEST(file_test, posLoadWordlist) {
   EXPECT_GT(ps.getWordlist().size(), 0);
 }
 
+/**
+ * @brief Test analysis of Sentiment class for detection of negative words given stub histogram simulating voice input. 
+ *
+ */
+TEST(input,  NegAnalysis) {
+  Parser  p;
+  NegativeSentiment ns;
+  ns.loadWordlist();
+  
+
+
+  std::vector<string> words;
+  words.push_back("GUESS");
+  words.push_back("HANGMAN");
+  words.push_back("DIFFICULT");
+  words.push_back("TOM");
+  words.push_back("JOB");
+  words.push_back("HELLO");
+  words.push_back("HELLO");
+  words.push_back("HELLO");
+  words.push_back("GUESS");
+
+  std::map<string , int > testMap = p.generateHistogram(words);
+  EXPECT_EQ(testMap["GUESS"] , 2);
+  EXPECT_EQ(testMap["HANGMAN"] , 1);
+  EXPECT_EQ(testMap["DIFFICULT"] , 1);
+  EXPECT_EQ(testMap["TOM"] , 1);
+  EXPECT_EQ(testMap["JOB"] , 1);
+  EXPECT_EQ(testMap["HELLO"] , 3);
+
+  ns.analysis(testMap);
+  int negativeEmotionScore = ns.getEmotionScore();
+  EXPECT_GT(negativeEmotionScore, 0);
+
+
+}
+
+
+
+/**
+ * @brief Test analysis of Sentiment class for specific number of negative words. 
+ *
+ */
+TEST(input,  exactWordScoreNegative) {
+  Parser  p;
+  NegativeSentiment ns;
+  ns.loadWordlist();
+
+
+
+  std::vector<string> words;
+  words.push_back("GUESS");
+  words.push_back("HANGMAN");
+  words.push_back("DIFFICULT");
+  words.push_back("TOM");
+  words.push_back("HELLO");
+  words.push_back("HELLO");
+  words.push_back("GUESS");
+
+  std::map<string , int > testMap = p.generateHistogram(words);
+
+  ns.analysis(testMap);
+  int negativeEmotionScore = ns.getEmotionScore();
+  EXPECT_EQ(negativeEmotionScore, 1);
+
+}
+
+
+
+
+/*
+ * @brief  analysis of Sentiment class for specific number of positive  words. 
+ *
+ */
+TEST(input,  exactWordScorePositive) {
+  Parser  p;
+  NegativeSentiment ns;
+  ns.loadWordlist();
+
+
+
+  std::vector<string> words;
+  words.push_back("LOVE");
+  words.push_back("HELLO");
+  words.push_back("HELLO");
+  words.push_back("GUESS");
+
+  std::map<string , int > testMap = p.generateHistogram(words);
+
+  ns.analysis(testMap);
+  int negativeEmotionScore = ns.getEmotionScore();
+  EXPECT_EQ(negativeEmotionScore, 1);
+
+}
+
+
+/**
+ * @brief Test analysis of Sentiment class for detection of positive  words given stub histogram simulating voice input. 
+ *
+ */
+
+TEST(input,  posAnalysis) {
+  Parser  p;
+  PositiveSentiment ps;
+  ps.loadWordlist();
+
+
+
+  std::vector<string> words;
+  words.push_back("GUESS");
+  words.push_back("HANGMAN");
+  words.push_back("DIFFICULT");
+  words.push_back("TOM");
+  words.push_back("JOB");
+  words.push_back("HELLO");
+  words.push_back("HELLO");
+  words.push_back("HELLO");
+  words.push_back("GUESS");
+
+  std::map<string , int > testMap = p.generateHistogram(words);
+  EXPECT_EQ(testMap["GUESS"] , 2);
+  EXPECT_EQ(testMap["HANGMAN"] , 1);
+  EXPECT_EQ(testMap["DIFFICULT"] , 1);
+  EXPECT_EQ(testMap["TOM"] , 1);
+  EXPECT_EQ(testMap["JOB"] , 1);
+  EXPECT_EQ(testMap["HELLO"] , 3);
+
+  ps.analysis(testMap);
+  int positiveEmotionScore = ps.getEmotionScore();
+  EXPECT_GT(positiveEmotionScore, 0);
+
+
+}
+
+
+
 
 int main(int argc, char **argv) {
 
